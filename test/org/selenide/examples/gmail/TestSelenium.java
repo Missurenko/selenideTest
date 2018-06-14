6debaf71fee1842cc06c6f58eee6123a588aa53b
bohdan.missurenko <bohdan.missurenko@limestonedigital.co>
2018-06-14 13:35:53 +0300

    done

--- test/org/selenide/examples/gmail/TestSelenium.java
+++ test/org/selenide/examples/gmail/TestSelenium.java
@@ -27,21 +27,7 @@ public class TestSelenium {
         String baseUrl = "http://www.google.co.uk/";
         driver.get(baseUrl);
         try {
-            driver.findElement(By.linkText("Увійти")).click();
-            driver.findElement(By.cssSelector("input")).sendKeys(loginName);
-            driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-            driver.findElement(By.className("qhFLie")).click();
-            driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-            driver.findElement(By.name("password")).sendKeys(pass);
-            driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-            Thread.sleep(2000);
-            driver.findElement(By.className("RveJvd")).click();
-            driver.findElement(By.className("gb_Sc")).findElement(By.cssSelector("a")).click();
-            driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-            driver.findElement(By.partialLinkText("Диск")).click();
-            driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-            JavascriptExecutor executor = (JavascriptExecutor) driver;
-            driver.findElement(By.className("l-u-Ab-zb-Pn-ve"));
+
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
